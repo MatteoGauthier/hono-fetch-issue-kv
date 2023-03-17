02e2cb74ef6488f1928b3f041b22790d36120e2d
Mattèo Gauthier <matteo.gauthier@gmail.com>
2023-03-17 14:07:27 +0100

    setup the example issue

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import { Hono } from "hono"
+import { cors } from "hono/cors"
+import { zValidator } from "@hono/zod-validator"
+import { z } from "zod"
+
+type Env = {
+  TEST_NAMESPACE: KVNamespace
+}
+
+// @xxx response.clone() issue maybe caused by zValidatior
+
+const app = new Hono<{ Bindings: Env }>()
+app.use(
+  "/*",
+  cors({
+    origin: "*",
+  })
+)
+
+app.post(
+  "/hello",
+  zValidator(
+    "json",
+    z.object({
+      name: z.string(),
+      email: z.string().email(),
+    })
+  ),
+  async (c) => {
+    const { email, name } = c.req.valid("json")
+
+    const existingUser = await c.env.TEST_NAMESPACE.get(email, "text")
+
+    if (existingUser) {
+      return c.json({
+        message: "You have already been greeted",
+      })
+    }
+
+    await c.env.TEST_NAMESPACE.put(email, name, {
+      metadata: {
+        name,
+        email,
+      },
+    })
+
+    return c.json({ message: "Hello" })
+  }
+)
+
+export default app
+
+// Generate a curl command to test the endpoint
+// curl -X POST -H "Content-Type: application/json" -d '{"name":"John Doe","email":"matt@gmail.com"}' https://0.0.0.0:8787/hello
